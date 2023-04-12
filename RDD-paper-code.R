2ae863f7965da43fba57b66c61debd64de3ee0ea
AdinaZekiAlHazzouri <64879845+AdinaZekiAlHazzouri@users.noreply.github.com>
2023-04-12 14:01:10 -0400

    Add files via upload

---
+++ RDD-paper-code.R
@@ -0,0 +1,160 @@
+######################################################################################################
+## BMJ
+## RD example code in R for "Regression Discontinuity Design studies: A guide for health researchers"
+## Authors: Sebastian Calonico, Neal Jawadekar, Katrina Kezios, Adina Zeki Al Hazzouri
+## Last update: April 11, 2023
+######################################################################################################
+## TO INSTALL/DOWNLOAD R PACKAGES:
+## RDROBUST (estimation and inference):  install.packages('rdrobust')
+## RDDENSITY (validation test):          install.packages('rddensity')
+######################################################################################################
+rm(list=ls())
+
+# Load R packages
+library(rdrobust)
+library(rddensity)
+
+### Load dataset
+setwd("INSERT DIRECTORY HERE") # Put location of your working directory here
+
+frm <-read.csv("frmgham2.csv")
+# For illustrative purposes, and to keep samples consistent between Sharp RD and Fuzzy RD models, we will limit our sample  
+# to individuals with a non-missing value of the outcome variable, running variable, and actual treatment take-up indicator.
+frm <- frm[complete.cases(frm[c("CVD", "SYSBP", "BPMEDS")]), ]
+
+######################################################################################################
+##### Step 1. Identify main components of the RD model ###############################################
+######################################################################################################
+
+Y = frm$CVD    # outcome variable
+X = frm$SYSBP  # running variable 
+D = frm$BPMEDS # actual treatment take-up indicator
+C = 140        # cutoff value
+T = 1*(X>=C)   # treatment assignment indicator 
+
+summary(Y)
+summary(X)
+summary(D) 
+summary(T)
+
+
+######################################################################################################
+##### Step 2. RD Plots: Exploratory plot to illustrate the nature of the relationship ################
+######################################################################################################
+
+rdplot1 = rdplot(y = Y, x = X, c = C, 
+                 x.label = "Systolic Blood Pressure (mmHg)", 
+                 y.label = "Cardiovascular Disease at Follow-up", col.dots="lightsteelblue3",col.lines="mediumorchid4")
+summary(rdplot1)
+library(ggplot2)
+
+
+######################################################################################################
+##### Step 3. Set up the local polynomial model ######################################################
+######################################################################################################
+P  = 1             # local linear approximation
+K  = 'triangular'  # triangular kernel  
+BW = 'mserd'       # MSE optimal bandwidth
+
+
+######################################################################################################
+##### Steps 4-5. RD Estimation and Inference #########################################################
+######################################################################################################
+rd_manual = rdrobust(y = Y, x = X, c = C, 
+                     kernel = K, p = P, bwselect = BW)
+summary(rd_manual)
+
+# You can also run RD using default choices, as shown here:
+rd_default = rdrobust(y = Y, x = X, c = C)
+summary(rd_default)
+
+
+### Plotting the RD model
+bw = rdrobust(y = Y, x = X, c = C, 
+              kernel = 'triangular', p = 1, bwselect = 'mserd')$bws[1]
+RD_plot = rdplot(y = Y, x = X, c = C,
+                 p = 1, kernel = 'triangular', h = bw,
+                 x.label = "Systolic Blood Pressure (mmHg)", 
+                 y.label = "Cardiovascular Disease at Follow-up",  subset = (X >= (C - bw) & X <= (C + bw)), x.lim = c(C - bw - 1,+ C + bw + 1), y.lim = c(0,+ 1), col.dots="lightsteelblue",col.lines="mediumorchid4")
+
+
+######################################################################################################
+##### Step 6. Robustness checks and validation #######################################################
+######################################################################################################
+
+# Change to uniform kernel
+rd_2 <- rdrobust(y = Y, x = X, c = C, 
+                 kernel = 'uniform', p = 1, bwselect = 'mserd')
+summary(rd_2)
+
+# Change to local quadratic regression
+rd_3 <- rdrobust(y = Y, x = X, c = C,
+                 kernel = 'triangular', p = 2, bwselect = 'mserd')
+summary(rd_3)
+
+# Change to different bandwidths on each side:
+rd_4 <- rdrobust(y = Y, x = X, c = C,
+                 kernel = 'triangular', p = 1, bwselect = 'msetwo')
+summary(rd_4)
+
+# Other bandwidth choices:
+rd_bws <- rdbwselect(y = Y, x = X, c = C, all = TRUE)
+summary(rd_bws)
+
+
+# Test for manipulation of the running variable: rddensity test
+rdd <- rddensity(X = X, c = C)
+summary(rdd)
+
+# Figure
+plot <- rdplotdensity(rdd, X,
+                      xlabel = "Systolic Blood Pressure (mmHg)", ylabel = "Density", lcol = "mediumorchid4",pcol = "lightsteelblue3", histLineCol = "lightsteelblue3", histFillCol = "lightsteelblue3", CIcol = "lightsteelblue3")
+#summary(plot)
+
+
+## Covariate balance assessment (Age)
+rd_covs = rdrobust(y = frm$AGE, x = X, c = C)
+summary(rd_covs)
+
+rdplot(y = frm$AGE, x = X, c = C, 
+  title = "", 
+  x.label = "Systolic Blood Pressure (mmHg)", 
+  y.label = "Age", col.dots="lightsteelblue3",col.lines="mediumorchid4")
+
+
+
+######################################################################################################
+#### Step 7: Fuzzy RD: average effect for compliers ##################################################
+######################################################################################################
+
+# Fuzzy RD model: 
+
+RD_fuzzy = rdrobust(y = Y, x = X, c = C, fuzzy = D)
+summary(RD_fuzzy)
+RD_fuzzy$Estimate[1] # Fuzzy RDD effect = -0.2301855
+
+
+# Next, manually calculate this fuzzy RDD effect by running the below 2 steps:
+# First, assess sharp RDD effect within bandwidth from the above fuzzy model
+RD_sharp = rdrobust(y = Y, x = X, c = C, h = RD_fuzzy$bws[1,1], b = RD_fuzzy$bws[2,1], subset=!is.na(D))
+summary(RD_sharp) 
+RD_sharp$Estimate[1] # Sharp RDD effect = -0.005580354
+
+# Next, assess compliance by computing RD effect using D as the outcome
+RD_comp = rdrobust(y = D, x = X, c = C, h = RD_fuzzy$bws[1], b = RD_fuzzy$bws[2,1])
+summary(RD_comp) 
+RD_comp$Estimate[1] # Compliance RDD effect = 0.02424286
+RD_comp_plot = rdplot(y = D, x = X, c = C, h = RD_fuzzy$bws[1,1], p=1, 
+                      x.label = "Systolic Blood Pressure (mmHg)", 
+                      y.label = "Use of anti-hypertensive medication", col.dots="lightsteelblue3",col.lines="mediumorchid4")
+
+# Final calculation:
+RD_sharp$Estimate[1] / RD_comp$Estimate[1] #  -0.2301855
+  
+
+                 
+
+
+
+
+
---
+++ RDD-paper-code.do
@@ -0,0 +1,132 @@
+**********************************************************************************************************
+** BMJ
+** RD example code in Stata for "Regression Discontinuity Design studies: A guide for health researchers"
+** Authors: Sebastian Calonico, Neal Jawadekar, Katrina Kezios, Adina Zeki Al Hazzouri
+** Last update: April 11, 2023
+**********************************************************************************************************
+** TO INSTALL/DOWNLOAD R PACKAGES:
+** RDROBUST (estimation and inference):  net install rdrobust, from(https://raw.githubusercontent.com/rdpackages/rdrobust/master/stata) replace
+** RDDENSITY (validation test):          net install rddensity, from(https://raw.githubusercontent.com/rdpackages/rddensity/master/stata) replace
+********************************************************************************
+clear all
+
+
+*** Load dataset
+cd "INSERT DIRECTORY HERE" /* Put location of your working directory here */
+use "frmgham2.dta"
+keep if !missing(cvd, sysbp, bpmeds) /* For illustrative purposes, and to keep samples consistent between Sharp RD and Fuzzy RD models, we will limit our sample  
+                                     /* to individuals with a non-missing value of the outcome variable, running variable, and actual treatment take-up indicator. */
+
+**********************************************************************************************************
+***** Step 1. Identify main components of the RD model ***************************************************
+**********************************************************************************************************
+
+global Y cvd    /* outcome variable */
+global X sysbp  /* running variable  */
+global D bpmeds /* actual treatment take-up indicator */
+global C 140        /* cutoff value */
+gen T = 1*($X>=$C)        /* treatment assignment indicator */
+
+su $Y $X $D T
+
+**********************************************************************************************************
+***** Step 2. RD Plots: Exploratory plot to illustrate the nature of the relationship ********************
+**********************************************************************************************************
+
+rdplot $Y $X, c($C) ///
+graph_options(title("") ///
+                     ytitle(Cardiovascular Disease at Follow-up) ///
+                     xtitle(Systolic Blood Pressure (mmHg)) ///
+                     graphregion(color(white)) legend(off))
+
+
+
+***********************************************************************************************************
+***** Step 3. Set up the local polynomial model ***********************************************************
+***********************************************************************************************************
+local P  = 1             /* local linear approximation */
+local K  = "triangular"  /* triangular kernel  */
+local BW = "mserd"       /* MSE optimal bandwidth*/
+
+
+***********************************************************************************************************
+***** Steps 4-5. RD Estimation and Inference **************************************************************
+***********************************************************************************************************
+rdrobust $Y $X, c($C) kernel(`K') p(`P') bwselect(`BW')
+
+* You can also run RD using default choices, as shown here:
+rdrobust $Y $X, c($C) 
+
+
+*** Plotting the RD model
+rdplot $Y $X if -e(h_l)<= $X-$C & $X-$C <= e(h_r), c($C) kernel(`K') p(`P') bwselect(`BW') ///
+					h(`e(h_l)' `e(h_r)')  ///
+					 graph_options(title("") ///
+                     ytitle(Cardiovascular Disease at Follow-up) ///
+                     xtitle(Systolic Blood Pressure (mmHg)) ///
+                     graphregion(color(white)) legend(off))
+
+
+**********************************************************************************************************
+***** Step 6. Robustness checks and validation ***********************************************************
+**********************************************************************************************************
+
+* Change to uniform kernel
+rdrobust $Y $X, c($C) kernel("uniform") p(1) bwselect("mserd")
+
+* Change to local quadratic regression
+rdrobust $Y $X, c($C) kernel("triangular") p(2) bwselect("mserd")
+
+* Change to different bandwidths on each side:
+rdrobust $Y $X, c($C) kernel("triangular") p(1) bwselect("msetwo")
+
+
+* Other bandwidth choices:
+rdbwselect $Y $X, c($C) all
+
+
+* Test for manipulation of the running variable: rddensity test
+rddensity $X, c($C) plot ///
+					 graph_opt(title("") ///
+                     ytitle(Cardiovascular Disease at Follow-up) ///
+                     xtitle(Systolic Blood Pressure (mmHg)) ///
+                     graphregion(color(white)) legend(off))
+
+
+
+** Covariate balance assessment (Age)
+rdrobust age $X, c($C)
+
+rdplot age $X, c($C) ///
+					 graph_options(title("Covariate Balance - RD Plot") ///
+                     ytitle(Age) ///
+                     xtitle(Systolic Blood Pressure (mmHg)) ///
+                     graphregion(color(white)) legend(off))
+
+
+
+**********************************************************************************************************
+**** Step 7: Fuzzy RD: average effect for compliers ******************************************************
+**********************************************************************************************************
+
+* First, assess compliance by computing RD effect using D as the outcome
+
+rdrobust $D $X, c($C)
+
+rdplot $D $X, c($C) ///
+					 graph_options(title("First Stage") ///
+                     ytitle(Use of anti-hypertensive medication) ///
+                     xtitle(Systolic Blood Pressure (mmHg)) ///
+                     graphregion(color(white)) legend(off))
+					 
+
+
+* Fuzzy RD model: 
+rdrobust $Y $X, c($C) fuzzy($D)
+
+
+
+
+
+
+
